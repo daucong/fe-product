a71c1b73d72cb5eff2bf4a1ea7df0273da01f75a
Admin <dauthaicong11092001@gmail.com>
2022-11-01 16:50:32 +0700

    update 11/1

--- src/main/java/com/example/feproduct/entity/Product.java
+++ src/main/java/com/example/feproduct/entity/Product.java
@@ -5,56 +5,14 @@ import lombok.Data;
 @Data
 public class Product {
     private Integer id;
+
     private String name;
+
     private float price;
+
     private String image;
+
     private Category category;
+
     private String category_id;
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getCategory_id() {
-        return category_id;
-    }
-
-    public void setCategory_id(String category_id) {
-        this.category_id = category_id;
-    }
 }
