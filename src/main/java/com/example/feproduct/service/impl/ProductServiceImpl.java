802f46009ae80d4c4050c4e2a33bc4474f5a99d0
Admin <dauthaicong11092001@gmail.com>
2022-11-02 09:34:52 +0700

    upload image to folder

--- src/main/java/com/example/feproduct/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/feproduct/service/impl/ProductServiceImpl.java
@@ -4,6 +4,8 @@ package com.example.feproduct.service.impl;
 import com.example.feproduct.client.ProductClient;
 import com.example.feproduct.entity.Product;
 import com.example.feproduct.service.ProductService;
+import com.example.feproduct.utils.ImageUpload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +16,8 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
     ProductClient productClient = new ProductClient();
+    @Autowired
+    private ImageUpload imageUpload;
 
     @Override
     public List<Product> getAllProduct() {
@@ -36,11 +40,13 @@ public class ProductServiceImpl implements ProductService {
             }
         } else {
             try {
+                imageUpload.uploadImage(imageProduct);
                 product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         productClient.saveProduct(product);
     }
 
